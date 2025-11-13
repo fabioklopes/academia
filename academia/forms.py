2889170ffda6434cb65a43af72174698fe2b9d1f
Fabio Klevinskas Lopes <fabioklopes@live.com>
2025-11-13 13:43:04 -0300

    Adaptação de formulário: Gestão de Graduações

---
+++ academia/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Graduacao
+
+class GraduacaoForm(forms.ModelForm):
+    class Meta:
+        model = Graduacao
+        fields = ['aluno', 'faixa', 'grau', 'data_graduacao']
+        widgets = {
+            'aluno': forms.Select(attrs={'class': 'form-select'}),
+            'faixa': forms.Select(attrs={'class': 'form-select'}),
+            'grau': forms.NumberInput(attrs={'class': 'form-control'}),
+            'data_graduacao': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+        }
