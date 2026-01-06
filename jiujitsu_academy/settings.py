85059bc69caa99545bea70ea35a5f35ec2090e4e
Fábio Klevinskas Lopes <fabioklopes@live.com>
2026-01-06 15:28:22 -0300

    ROLLBACK <- Configuration: disable DEBUG

--- jiujitsu_academy/settings.py
+++ jiujitsu_academy/settings.py
@@ -21,7 +21,7 @@ dotenv.load_dotenv(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'crtnbelem-adpm')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() in ['true', '1', 't']
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*',
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,15 +125,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
 
 # Media files
 MEDIA_URL = '/media/'
--- jiujitsu_academy/urls.py
+++ jiujitsu_academy/urls.py
@@ -2,23 +2,17 @@
 URL configuration for jiujitsu_academy project.
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('academia.urls')),
 ]
 
-# Configuração para servir arquivos de mídia em Desenvolvimento e Produção
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if not settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Configuração de handlers de erro
 handler404 = 'academia.views.error_404'
