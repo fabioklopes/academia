4858b5e9c48192cfcd70827ff10cdad38fe15148
Fábio Klevinskas Lopes <fabioklopes@live.com>
2026-01-08 16:03:13 -0300

    Resource: access request fields with mandatory completion.

--- academia/forms.py
+++ academia/forms.py
@@ -13,7 +13,7 @@ class SolicitacaoAcessoForm(forms.Form):
     first_name = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
     last_name = forms.CharField(max_length=150, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    whatsapp = forms.CharField(max_length=20, required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    whatsapp = forms.CharField(required=True, max_length=20, widget=forms.TextInput(attrs={'class': 'form-control'}))
     # Adicionado 'required': 'required' para validação do navegador
     birthday = forms.DateField(
         required=True, 
@@ -21,7 +21,7 @@ class SolicitacaoAcessoForm(forms.Form):
     )
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), required=True)
     password_confirm = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), required=True)
-    photo = forms.ImageField(required=False)
+    photo = forms.ImageField(required=True)
     has_responsible = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
     responsible_email = forms.EmailField(required=False, widget=forms.EmailInput(attrs={'class': 'form-control'}))
 
