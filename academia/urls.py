4f88ffccdfb56524c9ad81cbfff1d6a1920784e2
Fábio Klevinskas Lopes <fabioklopes@live.com>
2026-02-06 17:15:40 -0300

    Bugfix: correção do link de reset de senha

--- academia/urls.py
+++ academia/urls.py
@@ -21,7 +21,12 @@ urlpatterns = [
     path('switch-account-back/', views.switch_account_back, name='switch_account_back'),
     
     # Password Reset
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='academia/password_reset.html', form_class=CustomPasswordResetForm), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='academia/password_reset.html',
+        form_class=CustomPasswordResetForm,
+        email_template_name='academia/password_reset_email.html',
+        subject_template_name='academia/password_reset_subject.txt'
+    ), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='academia/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='academia/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='academia/password_reset_complete.html'), name='password_reset_complete'),
